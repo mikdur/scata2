910e61339a1853ae3694ec771c8ea4b133939bb1
Mikael Durling <mikael@naetet.net>
2025-03-12 12:29:40 +0100

    - Added basic infrastructure for user authenticatin.

--- scata_web/urls.py
+++ scata_web/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import django.contrib.auth
+from scata2 import urls as scata2_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('scata2/', include(scata2_urls)),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
