af802af309500f700c92c214fb67a61bd92cb982
Mikael Durling <mikael@durling.io>
2025-09-25 22:46:40 +0200

    - Some general (flake8) tidyness.

--- scata2/methods/__init__.py
+++ scata2/methods/__init__.py
@@ -1,10 +1,11 @@
 
 import scata2.methods.scata as scata
-from scata2.methods.dummy.models import DummyMethod
+import scata2.methods.dummy as dummy
 
-methods = { "scata" : { "model" : scata.models.ScataMethod,
-                        "description": "Scata"},
-            "dummy" : { "model" : DummyMethod ,
-                        "description" : "Dummy test method" },
-}
-
+methods = {"scata": {"model": scata.models.ScataMethod,
+                     "form": scata.models.ScataMethodForm,
+                     "description": "Scata"},
+           "dummy": {"model": dummy.models.DummyMethod,
+                     "form": dummy.models.DummyMethodForm,
+                     "description": "Dummy test method"},
+           }
