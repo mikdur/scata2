b1359d7da690550092e7d93ebcbdc622a729bd4f
Mikael Durling <mikael@durling.io>
2025-11-06 09:59:34 +0100

    - Avoid cyclic import dependencies

--- scata2/backend/__init__.py
+++ scata2/backend/__init__.py
@@ -1,6 +1,6 @@
-from .tagset import parse_tagset
-from .file import check_file
-from .dataset import check_dataset
-from .referenceset import check_refset
-from .dataset_stats import dataset_stats
-from .job import run_job
+# from .tagset import parse_tagset
+# from .file import check_file
+# from .dataset import check_dataset
+# from .referenceset import check_refset
+# from .dataset_stats import dataset_stats
+# from .job import run_job
